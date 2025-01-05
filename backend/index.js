c5a27cd4fd441194731a4bca9009ba71f83a0205
AradhayJain <153743476+AradhayJain@users.noreply.github.com>
2025-01-05 22:42:25 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -31,9 +31,9 @@ app.listen(PORT, () => {
 
 const __dirname=path.resolve()
 if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,'frontend', 'dist')))
+    app.use(express.static(path.join(__dirname,'client', 'dist')))
     app.get("*",(req,res)=>res.sendFile 
-    (path.resolve(__dirname,"frontend","dist", "index.html")))
+    (path.resolve(__dirname,"client","dist", "index.html")))
 }
 else{
     app.get("/",(req,res)=>{
